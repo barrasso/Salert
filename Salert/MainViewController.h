60dd0288193e8477127cf295f10e43fd25719ac2
Mark Barrasso <mark.barrasso@live.com>
2014-11-18 18:25:12 -0500

    initial commit

---
+++ Salert/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Salert
+//
+//  Created by Mark on 11/18/14.
+//  Copyright (c) 2014 MEB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
